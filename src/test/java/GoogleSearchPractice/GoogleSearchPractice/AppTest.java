bc4cb8ce3034ed7b15c773e0a33d6d0860beb243
Sibghatullah Veedy <sibghatullahveedy@gmail.com>
2020-07-03 12:59:35 -0700

    google search repo

---
+++ src/test/java/GoogleSearchPractice/GoogleSearchPractice/AppTest.java
@@ -0,0 +1,20 @@
+package GoogleSearchPractice.GoogleSearchPractice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
